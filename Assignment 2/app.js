f3aedc0dbdab9aa7ed3cec8167e7e5b2ea57c7be
Mars311 <mabdulrahim75@gmail.com>
2017-03-10 16:57:49 +0500

    Assignment 2

---
+++ Assignment 2/app.js
@@ -0,0 +1,23 @@
+var a = +prompt ("Enter you percentage in number");
+
+if (a >= 80)
+{
+    alert ("Congratulations! Your Grade is A+");
+
+}
+else if (a >= 70)
+{
+    alert ("Congratulations! Your Grade is A");
+}
+else if (a >= 60)
+{
+    alert ("Congratulations! your Grade is B");
+}
+else if (a >= 50)
+{
+    alert ("Your Grade is C");
+}
+else if (a < 50)
+{
+    alert ("Sorry! You are Failed");
+}
